57bcc5f62116ff3f9d42eb5cf0a2dece8a80f420
Somraj Saha Roy <somrajroy20@gmail.com>
2022-09-01 01:34:53 +0530

    first commit

---
+++ todo_feature.js
@@ -0,0 +1,65 @@
+getUpdate = () => {
+    item = document.getElementById("items").value;
+    desc = document.getElementById("description").value;
+    if (localStorage.getItem("itemsJson") == null) {
+        jsonItemsArray = [];
+        jsonItemsArray.push([item, desc]);
+        localStorage.setItem("itemsJson", JSON.stringify(jsonItemsArray));
+    }
+    else {
+        itemArraystr = localStorage.getItem("itemsJson");
+        jsonItemsArray = JSON.parse(itemArraystr);
+        jsonItemsArray.push([item, desc]);
+        localStorage.setItem("itemsJson", JSON.stringify(jsonItemsArray));
+    }
+    update();
+
+}
+update = () => {
+    if (localStorage.getItem("itemsJson") == null) {
+        jsonItemsArray = [];
+        localStorage.setItem("itemsJson", JSON.stringify(jsonItemsArray));
+    }
+    else {
+        itemArraystr = localStorage.getItem("itemsJson");
+        jsonItemsArray = JSON.parse(itemArraystr);
+    }
+    // for populating the table
+    let body = document.getElementById("tablebody");
+    let str = "";
+    jsonItemsArray.forEach((element, index) => {
+        str += `
+        <tr>
+        <th scope="row">${index + 1}</th>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td>
+        <td><button class="btn btn-sm btn-danger" onClick="deleteItems(${index})">Delete</button></td>
+      </tr>
+        `
+    });
+    body.innerHTML = str;
+}
+deleteItems = (itemsIndex) => {
+    console.log("deleted item", itemsIndex);
+    itemArraystr = localStorage.getItem("itemsJson");
+    jsonItemsArray = JSON.parse(itemArraystr);
+    // deleting element with a given index
+    jsonItemsArray.splice(itemsIndex, 1);
+    //storing the updated array in localstorage in string format
+    localStorage.setItem("itemsJson", JSON.stringify(jsonItemsArray));
+    update();
+}
+clearItem = () => {
+    if (confirm("Do You Want To Clear The List")) {
+        localStorage.clear();
+        update();
+    }
+}
+// if (localStorage.getItem("itemsJson") != null) {
+//     update();
+// }
+
+addItems = () => {
+    getUpdate();
+}
+update();
